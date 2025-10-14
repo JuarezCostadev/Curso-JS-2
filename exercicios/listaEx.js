3bb0b8cd3bbf1a840dbaf85da633f8bd3f6fc887
JuarezCosta <juarezcostasilva1999@outlook.com>
2025-10-14 20:28:57 -0300

    Create listaEx.js

---
+++ exercicios/listaEx.js
@@ -0,0 +1,4 @@
+let listaGenerica = []
+let linguagemDeProgramacao = ['JavaScript', 'C', 'C++','Kotlin', 'Python']
+linguagemDeProgramacao.push('Java', 'Ruby', 'GoLang')
+console.log(linguagemDeProgramacao[0])
